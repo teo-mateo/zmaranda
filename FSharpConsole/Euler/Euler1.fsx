b203a3b8461bb3028d235c4a0f2d497fab66d302
Teodor B <tbardici@gmail.com>
2016-03-09 06:40:44 +0100

    F# project

---
+++ FSharpConsole/Euler/Euler1.fsx
@@ -0,0 +1,2 @@
+﻿
+List.sum(List.filter(fun x -> x%3=0 || x%5=0) [0..999])
---
+++ FSharpConsole/Euler/Euler2.fsx
@@ -0,0 +1,19 @@
+﻿let rec fib (x:int) =
+    let ret = match x with
+        | 1 -> 1
+        | 2 -> 2
+        | _ -> 
+            let y = x-1
+            let z = x-2
+            (fib y) + (fib z)
+    ret
+
+let fib2 limit= 
+    let mutable t= (1,1)
+    let mutable sum=0
+    while snd t < limit do
+        let nxt = fst t + snd t
+        t <- (snd t, nxt)
+        if nxt%2=0 then sum<-sum+nxt
+
+    sum
