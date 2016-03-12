881884552e3369b18d9ebe358dc484ede0375f0a
Teodor B <tbardici@gmail.com>
2016-03-12 09:38:20 +0100

    more exper f#

---
+++ FSharpConsole/Expert3.fsx
@@ -0,0 +1,20 @@
+﻿open System.Net
+open System.IO
+
+let http (url: string) = 
+    let req = WebRequest.Create(url)
+    let resp = req.GetResponse()
+    let stream = resp.GetResponseStream()
+    let reader = new StreamReader(stream)
+    let html = reader.ReadToEnd()
+    resp.Close()
+    html
+
+let delimiters = [|' '; '\n'; '\t'; '<'; '>'; '='|]
+let getWords (s:string) = s.Split delimiters
+
+let google = http "http://google.com"
+
+//google |> getWords |> Array.filter (fun s-> s="href") |> Array.length
+let countLinks = getWords >> Array.filter(fun s-> s="href") >> Array.length
+google |> countLinks
---
+++ FSharpConsole/ExpertFsharp2.fsx
@@ -0,0 +1,27 @@
+﻿open System.Net
+open System.IO
+
+let http (url: string) = 
+    let req = WebRequest.Create(url)
+    let resp = req.GetResponse()
+    let stream = resp.GetResponseStream()
+    let reader = new StreamReader(stream)
+    let html = reader.ReadToEnd()
+    resp.Close()
+    html
+
+let delimiters = [|' '; '\n'; '\t'; '<'; '>'; '='|]
+let getWords (s:string) = s.Split delimiters
+let getStats site = 
+    let url = "http://" + site
+    let html = http url
+    let hwords = html |> getWords
+    let hrefs = html |> getWords |> Array.filter (fun s -> s = "href")
+
+    (site, html.Length, hwords.Length, hrefs.Length)
+
+let sites = ["bing.com"; "google.com"; "search.yahoo.com"]
+
+List.map getStats sites
+//alternative syntax
+//sites |> List.map getStats
