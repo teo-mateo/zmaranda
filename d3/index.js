bab64a71cccc7783309162f73708d47f0f5f2541
Teodor B <teomati@gmail.com>
2016-04-23 20:36:37 +0200

    d3

---
+++ d3/index.js
@@ -0,0 +1,18 @@
+var testString = "alfa beta gama";
+
+console.log("yo man");
+function fuckit(argument) {
+	console.log("fuck the argument");
+}
+
+function add(a, b){
+	return a+b;
+}
+
+function subtract(a, b){
+	return a-b;
+}
+
+function alpha(){
+	fuckit(1);
+}
