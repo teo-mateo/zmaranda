2092fd0443c81324720c60507ecdc6ee880d253d
Teodor B <tbardici@gmail.com>
2016-06-26 01:50:58 +0200

    ccd

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -24,6 +25,10 @@ namespace ConsoleApp1
             {
                 Console.WriteLine(i.Name);
             }
+
+            Console.ReadKey();
+
+            Console.WriteLine(JsonConvert.SerializeObject(items, Formatting.Indented));
 
             Console.ReadKey();
         }
